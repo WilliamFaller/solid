938c87597306b98ac49f32f8de77f79eaefc66a0
WilliamFaller <mew.pvp10@gmail.com>
2023-09-04 19:24:18 -0300

    Configurando testes unitarios

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -22,9 +22,7 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
     if (error instanceof UserAlreadyExistsError) {
       return reply.status(409).send({ message: error.message })
     }
-
     throw error
-    return reply.status(500).send() // TODO: Create a generic error handler
   }
 
   return reply.status(201).send()
